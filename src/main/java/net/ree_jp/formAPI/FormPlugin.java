f8012a6d6bb3d6340771d3fbc0b8f340274e2ee1
Ree_ <mail@ree-jp.net>
2020-08-11 00:04:19 +0900

    update: java 1.8

--- src/main/java/net/ree_jp/formAPI/FormPlugin.java
+++ src/main/java/net/ree_jp/formAPI/FormPlugin.java
@@ -14,7 +14,7 @@ package net.ree_jp.formAPI;
 import cn.nukkit.plugin.PluginBase;
 import net.ree_jp.formAPI.event.FormListener;
 
-class FormPlugin extends PluginBase {
+public class FormPlugin extends PluginBase {
 
     @Override
     public void onEnable() {
